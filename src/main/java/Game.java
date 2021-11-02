c031d7deb0ea24a439ee7c729cc84d846fa49b2f
david <davidespinhamarques@gmail.com>
2021-11-02 15:53:08 +0000

    a meio do 6

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,3 @@
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,28 +5,27 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
-public class Application {
+public class Game {
     public static void main(String[] args) {
         try {
-
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
             Screen screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
-
-            //TerminalSize terminalSize = new TerminalSize(40, 20);
-            //DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            //Terminal terminal2 = terminalFactory.createTerminal();
-
-            screen.clear();
-            screen.setCharacter(10, 10,new TextCharacter('x'));
-            screen.refresh();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    class screen {
+        private void draw(){
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        }
+        public void run(){
+            draw();
+        }
     }
 }
-
