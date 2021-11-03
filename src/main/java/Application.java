c1080168e2f2bb0fc65bc5ebbef86c962fe3aac2
david <davidespinhamarques@gmail.com>
2021-11-03 18:36:12 +0000

    meio exercicio feito

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,30 +4,14 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.TextCharacter;
 
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-            //TerminalSize terminalSize = new TerminalSize(40, 20);
-            //DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            //Terminal terminal2 = terminalFactory.createTerminal();
-
-            screen.clear();
-            screen.setCharacter(10, 10,new TextCharacter('x'));
-            screen.refresh();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args)throws IOException {
+        Game game = new Game();
+        game.run();
 
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,6 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -6,10 +9,14 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    public static void main(String[] args) {
+    Screen screen;
+    private int x =10;
+    private int y =10;
+
+    public Game() {
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
+            screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
@@ -18,14 +25,29 @@ public class Game {
         }
     }
 
-    class screen {
-        private void draw(){
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
+    private void draw() throws IOException{
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        screen.clear();
+        screen.setCharacter(x , y ,new TextCharacter('x'));
+        screen.refresh();
+
+        KeyStroke key = screen.readInput();
+
+        processKey(key);
+
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'w'){
+            System.out.println("w is pressed");
         }
-        public void run(){
-            draw();
-        }
+    }
+    public void run() throws IOException{
+        draw();
+    }
+
+
+
+    private void processKey (KeyStroke key) {
+        System.out.println(key);
     }
 }
